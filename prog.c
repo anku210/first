0143355e57568bf8a54dd6d659bde681fb5254fd
anku210 <miniojha21093@gmail.com>
2019-12-05 06:39:37 -0800

    calculator program

---
+++ prog.c
@@ -0,0 +1,14 @@
+void main()
+{
+ int a,b,sum,sub,mul,div;
+printf("enter two numbers a and b");
+scanf("%d%d",&a &b);
+sum = a + b;
+sub = a - b;
+mul = a * b;
+div = a/b;
+printf("sum %d",sum);
+printf("sub is %d" , sub);
+printf("mul is %d",mul);
+printf("div is %d",div);
+} 
